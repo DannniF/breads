30418983296d096c5d25ab7ffadd2823c848bda3
Daniel Flores <your@example.com>
2023-11-28 18:58:51 -0800

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -8,17 +8,8 @@ breads.get('/',function(req, res){
         breads: Bread
          }
     )
-    // res.send(Bread)
 });
 
-//SHOW
-// breads.get('/:arrayIndex', function(req, res){
-//     res.render('Show', {
-//         bread: Bread[req.params.arrayIndex]
-    
-//     })
-// })
-// NEW
 breads.get('/new', (req, res) => {
     res.render('new')
 })
@@ -28,7 +19,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', function(req, res){
     if (Bread[req.params.arrayIndex]) {
       res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
+        bread:Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex,
       })
     } else {
       res.render('404')
@@ -49,7 +41,16 @@ breads.post('/', (req, res) => {
     res.redirect('/breads')
   })
   
-  
+
+
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
+
 module.exports = breads
 
 
--- server.js
+++ server.js
@@ -6,18 +6,22 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+
+// DEPENDENCIES
+const methodOverride = require('method-override')
+
 //Midd
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(methodOverride('_method'))
 
 //Routes
 app.get('/',function(req, res){
     res.send('Welcome to an Awesome App about Breads!')
 })
-
 
 
 // breads
