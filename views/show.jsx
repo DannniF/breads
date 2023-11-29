a2ce2cd2cff9d4f53e1f11381ecac92492e8c048
Daniel Flores <your@example.com>
2023-11-28 19:22:14 -0800

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -16,8 +16,13 @@ function Show ({bread, index}){
                 have Gluten
             </p>
             <img src={bread.image} alt={bread.name} />
-            <li><a href="/breads">Go home</a></li>
+            <li>
+                <a href="/breads">Go home</a>
+              
 
+            
+            </li>
+<a href={`/breads/${index}/edit`}><button>Edit</button></a>
             
 <form action={`/breads/${index}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
