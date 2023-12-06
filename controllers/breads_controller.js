fc6ed5f5954908cb6998362723f52ff7362e60fc
Daniel Flores <your@example.com>
2023-12-05 20:50:54 -0800

    added made by baker to show page

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -70,6 +70,8 @@ breads.get('/:id/edit', (req, res) => {
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
     .then(foundBread => {
+      const bakedBy = foundBread.getBakedBy()
+      console.log(bakedBy)
       res.render('show', {
         bread: foundBread
       })
